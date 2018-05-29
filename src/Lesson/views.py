6035b803a6cdde8e3f1ec1ee08c7b77aac0cb101
Farooq Azam Khan <farooq1.khan@ryerson.ca>
2018-05-28 22:22:31 -0400

    did Q lookup

--- src/Lesson/views.py
+++ src/Lesson/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
-
+from django.db.models import Q
 from .models import LessonPlan
 # def lesson_listview(request):
 #     template_name = 'Lesson/lesson.html'
@@ -18,8 +18,21 @@ class LessonListView(ListView):
     queryset = LessonPlan.objects.all()
 
 
-class EnglishLessonsListView(ListView):
+class SearchLessonsListView(ListView):
     model = LessonPlan
-    # paginate_by = 1
     template_name = "Lesson/lesson.html"
-    queryset = LessonPlan.objects.filter(subject__iexact="english")
+
+    def get_queryset(self):
+        #kwargs gets a dictionary
+        print(self.kwargs)
+        slug = self.kwargs.get("slug")
+        # print(slug)
+        if slug:
+            queryset = LessonPlan.objects.filter(
+                                            Q(subject__iexact=slug)|
+                                            Q(subject__icontains=slug)|
+                                            Q(lesson_name__iexact=slug)|
+                                            Q(lesson_name__icontains=slug))
+        else:
+            queryset = LessonPlan.objects.none()
+        return queryset
